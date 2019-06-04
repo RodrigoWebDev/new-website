dc4680ce4f808e3c9639529e16203b7d970073bd
Rodrigo <rodrigo.queiroz.chagas@gmail.com>
2019-06-04 19:27:19 -0300

    nav link active added

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,32 @@
 import React from "react"
 import {Link} from "gatsby"
 import perfilImg from "../img/rodrigo-queiroz.jpg"
+import $ from "jquery"
 
-export default() => (
-    <header className="header container" id="top">
-        <Link to="/">
-            <img title="Clique para ir para a home" className="perfil-img" src={perfilImg}/>
-            <h1 className="perfil-name">Rodrigo Queiroz</h1>
-        </Link>
-        <nav className="nav is-center">
-            <ul className="tabs">
-                <Link to="/portfolio">💻Portfólio</Link>
-                <Link to="/sobre">😀Sobre</Link>
-                <Link to="/empresas">🏢Empresas</Link>
-            </ul>
-        </nav>
-    </header>
-)
+export default class Header extends React.Component { 
+    componentDidMount(){
+        $(".tabs > a").click(function(){
+            if($(this).attr("href") === window.location.pathname){
+                this.addClass("active");
+            }
+        });
+    }
+
+    render(){
+        return(
+            <header className="header container" id="top">
+                <Link to="/">
+                    <img title="Clique para ir para a home" className="perfil-img" src={perfilImg}/>
+                    <h1 className="perfil-name">Rodrigo Queiroz</h1>
+                </Link>
+                <nav className="nav is-center">
+                    <ul className="tabs">
+                        <Link to="/portfolio">💻Portfólio</Link>
+                        <Link to="/sobre">😀Sobre</Link>
+                        <Link to="/empresas">🏢Empresas</Link>
+                    </ul>
+                </nav>
+            </header>
+        )
+    }
+}
