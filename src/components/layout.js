2173ef4248d48ad9e44e41d1d7320a1b5aaa24f1
Rodrigo <rodrigo.queiroz.chagas@gmail.com>
2019-06-04 19:16:10 -0300

    Whatsapp fixed button

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Header from "./header"
 import Footer from "./footer"
+import whatsapp from "../img/whatsapp-white.svg"
 
 export default({children}) => (
     <div>
         <Header/>
         <main className="main container">
             {children}
+            <a class="button success button-whatsapp"><span>Contato</span><img width="15" src={whatsapp}/> </a>
         </main>
         <Footer/>
     </div>
